eb2f3acf9b5bb6ac379c94cf2458e94233c96037
Hugo Mafra <hugorc10@hotmail.com>
2021-12-06 13:39:22 -0300

    Adicionando arquivos de exercicios

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -0,0 +1,10 @@
+let pizzas = ['4 Queijos', 'Frango com Catupiry', 'Marguerita', 'Palmito', 'Chocolate'];
+
+// pizzas[4] = 'Peito de Peru';
+
+pizzas.push('Peito de Peru');
+console.log(pizzas.length);
+
+console.log(pizzas.sort())
+
+// console.log('Menu de sabores ' + pizzas);
